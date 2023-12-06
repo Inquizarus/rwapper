22374ebed2930586092358f26c9a25770a439d29
Conny Karlsson <connykarlsson9@gmail.com>
2023-12-06 09:14:44 +0000

    Improved test coverage in general

--- middleware_test.go
+++ middleware_test.go
@@ -33,3 +33,79 @@ func TestThatChainMiddlewaresWorksAsIntended(t *testing.T) {
 	assert.Equal(t, testValue, response.Body.String())
 	assert.Equal(t, testValue, response.Header().Get(testHeaderKey))
 }
+
+func TestChainMiddlewareWithNoMiddlewares(t *testing.T) {
+	// Test with no middlewares
+	handler := rwapper.ChainMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(testValue))
+	}))
+
+	response := httptest.NewRecorder()
+
+	handler.ServeHTTP(response, httptest.NewRequest(http.MethodGet, "http://localhost", nil))
+
+	assert.Equal(t, testValue, response.Body.String())
+}
+
+func TestChainMiddlewareWithMultipleMiddlewares(t *testing.T) {
+
+	// Test with multiple middlewares
+	handler := rwapper.ChainMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(testValue))
+	}),
+		func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set(testHeaderKey, testValue)
+				if nil != next {
+					next.ServeHTTP(w, r)
+				}
+			})
+		},
+		func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusTeapot)
+				if nil != next {
+					next.ServeHTTP(w, r)
+				}
+			})
+		})
+
+	response := httptest.NewRecorder()
+
+	handler.ServeHTTP(response, httptest.NewRequest(http.MethodGet, "http://localhost", nil))
+
+	assert.Equal(t, testValue, response.Body.String())
+	assert.Equal(t, testValue, response.Header().Get(testHeaderKey))
+	assert.Equal(t, http.StatusTeapot, response.Result().StatusCode)
+}
+
+func TestChainMiddlewareAppliesMiddlewaresInCorrectOrder(t *testing.T) {
+
+	// Test the order of middleware execution
+	var executedMiddleware string
+	handler := rwapper.ChainMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(testValue))
+	}),
+		func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				executedMiddleware += "First "
+				if nil != next {
+					next.ServeHTTP(w, r)
+				}
+			})
+		},
+		func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				executedMiddleware += "Second "
+				if nil != next {
+					next.ServeHTTP(w, r)
+				}
+			})
+		})
+
+	response := httptest.NewRecorder()
+
+	handler.ServeHTTP(response, httptest.NewRequest(http.MethodGet, "http://localhost", nil))
+
+	assert.Equal(t, "First Second ", executedMiddleware)
+}
--- pkg/echowrapper/wrapper_test.go
+++ pkg/echowrapper/wrapper_test.go
@@ -1,6 +1,7 @@
 package echowrapper_test
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,30 +12,91 @@ import (
 )
 
 const (
-	testHeaderKey = "x-test-header"
-	testValue     = "test"
+	testHeaderKey      = "x-test-header"
+	testHeaderValue    = "test"
+	testParameterKey   = "foo"
+	testParameterValue = "bar"
+	bodyString         = "test body"
 )
 
 func TestThatTheWrapperWorksAsIntended(t *testing.T) {
 	wrapper := echowrapper.New(nil)
-	parameterKey := "bar"
 
-	wrapper.HandlerFunc(http.MethodGet, "/foo/"+wrapper.Parameterize(parameterKey), func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(wrapper.ParameterByName(parameterKey, r)))
-	}, func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set(testHeaderKey, testValue)
-			if nil != next {
-				next.ServeHTTP(w, r)
-			}
+	testCases := []struct {
+		method      string
+		routerPath  string
+		requestPath string
+		body        []byte
+		expected    string
+		headerKey   string
+		headerVal   string
+	}{
+		{
+			method:      http.MethodGet,
+			routerPath:  "/foo/" + wrapper.Parameterize(testParameterKey),
+			requestPath: "/foo/" + testParameterValue,
+			body:        nil,
+			expected:    testParameterValue,
+			headerKey:   testHeaderKey,
+			headerVal:   testHeaderValue,
+		},
+		{
+			method:      http.MethodPost,
+			routerPath:  "/foo/" + wrapper.Parameterize(testParameterKey),
+			requestPath: "/foo/" + testParameterValue,
+			body:        []byte("request body"),
+			expected:    testParameterValue,
+			headerKey:   testHeaderKey,
+			headerVal:   testHeaderValue,
+		},
+		{
+			method:      http.MethodPut,
+			routerPath:  "/foo/" + wrapper.Parameterize(testParameterKey),
+			requestPath: "/foo/" + testParameterValue,
+			body:        []byte("request body"),
+			expected:    testParameterValue,
+			headerKey:   testHeaderKey,
+			headerVal:   testHeaderValue,
+		},
+		{
+			method:      http.MethodDelete,
+			routerPath:  "/foo/" + wrapper.Parameterize(testParameterKey),
+			requestPath: "/foo/" + testParameterValue,
+			body:        []byte("request body"),
+			expected:    testParameterValue,
+			headerKey:   testHeaderKey,
+			headerVal:   testHeaderValue,
+		},
+		{
+			method:      http.MethodPatch,
+			routerPath:  "/foo/" + wrapper.Parameterize(testParameterKey),
+			requestPath: "/foo/" + testParameterValue,
+			body:        []byte("request body"),
+			expected:    testParameterValue,
+			headerKey:   testHeaderKey,
+			headerVal:   testHeaderValue,
+		},
+	}
+
+	for _, tc := range testCases {
+		// Register the route for the test case
+		wrapper.HandlerFunc(tc.method, tc.routerPath, func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(wrapper.ParameterByName(testParameterKey, r)))
+		}, func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set(testHeaderKey, testHeaderValue)
+				if nil != next {
+					next.ServeHTTP(w, r)
+				}
+			})
 		})
-	})
 
-	response := httptest.NewRecorder()
-	request := httptest.NewRequest(http.MethodGet, "http://localhost/foo/"+testValue, nil)
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(tc.method, "http://localhost"+tc.requestPath, bytes.NewReader(tc.body))
 
-	wrapper.ServeHTTP(response, request)
+		wrapper.ServeHTTP(response, request)
 
-	assert.Equal(t, testValue, response.Body.String())
-	assert.Equal(t, testValue, response.Header().Get(testHeaderKey))
+		assert.Equal(t, tc.expected, response.Body.String())
+		assert.Equal(t, tc.headerVal, response.Header().Get(tc.headerKey))
+	}
 }
